1078ea0f3515d226a0f73eaa73f4d099c640bc81
Nawabmeeravali <meeravalinawab7@gmail.com>
2020-05-20 07:55:04 +0530

    ntg

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def add_message():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=port,debug=True, use_reloader=True)
+    app.run(host= '0.0.0.0',port=port,debug=True, use_reloader=True)
